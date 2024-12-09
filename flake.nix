98f40511c535f35932fb7a4d0b2f9ef0b7042ccd
Braian A. Diez <bdiez19@gmail.com>
2024-12-08 22:13:11 -0300

    init: starting flake template

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Bug Bounty Hunter Default Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nmap
+          ];
+        };
+      });
+}
